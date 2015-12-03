49e9915565da7abb97ace79780954b35baa7d2c0
Kristóf Poduszló <kripod@protonmail.com>
2015-12-03 19:47:24 +0100

    Added support for deposit address retrieval; Updated readme

--- Source/Primedice.NET/Components/WalletManager.cs
+++ Source/Primedice.NET/Components/WalletManager.cs
@@ -17,13 +17,25 @@ namespace KriPod.Primedice.Components
         /// <summary>Withdraws a specific amount of satoshis to a given address.</summary>
         /// <param name="amount">Amount of satoshi to withdraw.</param>
         /// <param name="address">A Bitcoin address where the withdrawn funds should be sent.</param>
-        /// <returns>An awaitable <see cref="Withdrawal"/> object if the withdrawal was successful.</returns>
+        /// <returns>An awaitable <see cref="Withdrawal"/> object if the withdrawal was successful, or null if the current <see cref="PrimediceClient"/> is not authenticated.</returns>
         public async Task<Withdrawal> Withdraw(double amount, string address)
         {
+            if (!WebClient.IsAuthorized) return null;
+
             return await WebClient.Post<Withdrawal>("withdraw", new Dictionary<string, string> {
                 ["amount"] = amount.ToString(CultureInfo.InvariantCulture),
                 ["address"] = address
             });
+        }
+
+        /// <summary>Gets the deposit address which belongs to the current <see cref="PrimediceClient"/> instance.</summary>
+        /// <returns>An awaitable string, or null if the current <see cref="PrimediceClient"/> is not authenticated.</returns>
+        public async Task<string> GetDepositAddress()
+        {
+            if (!WebClient.IsAuthorized) return null;
+
+            var response = await WebClient.Get<DepositAddress>("deposit");
+            return response.Address;
         }
     }
 }
